d26c621c3492d50bd283e6af5a3f7d8e712d585a
MonoxTI <itumonokoane84@gmail.com>
2025-05-19 16:08:21 +0200

    Adding property to database

---
+++ Manger1/DataHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manger1
+{
+    internal class DataHandler
+    {
+        string connect = "Server = MONOX\\MSSQLSEERVER01 ; Inital Catalog = Propety; Integrated Security = SSPI";
+
+        public void Addprperty(string PropertyName, int Rent, int Levy, int BondInterest, int Rates, string Descrip, int Total, int Income, int Expenses, string TenantName, DateTime StartDate, DateTime EndDate)
+        {
+            using (SqlConnection conn = new SqlConnection(connect))
+            {
+                SqlCommand cmd = new SqlCommand("[dbo].AddProperty", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@PropertyName", PropertyName);
+                cmd.Parameters.AddWithValue("@Rent", Rent);
+                cmd.Parameters.AddWithValue("@Levy", Levy);
+                cmd.Parameters.AddWithValue("@BondInterest", BondInterest);
+                cmd.Parameters.AddWithValue("@RatesAndTaxes", Rates);
+                cmd.Parameters.AddWithValue("Description", Descrip);
+                cmd.Parameters.AddWithValue("@Total", Total);
+                cmd.Parameters.AddWithValue("@Income", Income);
+                cmd.Parameters.AddWithValue("@Tenant", TenantName);
+                cmd.Parameters.AddWithValue("@Expenses", Expenses);
+                cmd.Parameters.AddWithValue("@OwnershipStartDate", StartDate);
+                cmd.Parameters.AddWithValue("@OwnershipEndDate", EndDate);
+
+                conn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public DataTable getPrp()
+        {
+            SqlConnection conn = new SqlConnection(connect);
+            SqlDataAdapter adapter = new SqlDataAdapter("GetProp", conn)
+            return
+        }
+
+    }
+}
--- Manger1/Form1.cs
+++ Manger1/Form1.cs
@@ -12,9 +12,16 @@ namespace Manger1
 {
     public partial class Form1 : Form
     {
+        DataHandler handler = new DataHandler();
         public Form1()
         {
             InitializeComponent();
+            DataGridView.DataSource = AddingNewEventHandler.getproperty();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
